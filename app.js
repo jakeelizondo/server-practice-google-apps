7ffc4d254ad8ae82ee44d3b2d7262f91ab8d52c4
Jake Elizondo <jake.elizondo23@gmail.com>
2020-12-29 11:36:23 -0500

    created server

---
+++ app.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const googleData = require('./googleData');
+
+const app = express();
+app.use(morgan('dev'));
+
+const acceptedGenres = [
+  'Action',
+  'Puzzle',
+  'Strategy',
+  'Casual',
+  'Arcade',
+  'Card',
+];
+
+app.get('/', (req, res) => {
+  const { sort, genre } = req.query;
+  let results = googleData;
+
+  if (sort && sort !== 'Rating' && sort !== 'App') {
+    return res.status(400).send('Sort must be by "rating" or "app"');
+  }
+
+  if (genre && !acceptedGenres.includes(genre)) {
+    return res.status(400).send('Filter must be an accepted Genre');
+  }
+
+  results = results.filter((app) => app.Genres.includes(genre));
+
+  if (sort) {
+    results.sort((curr, next) => {
+      if (curr[sort] < next[sort]) {
+        return -1;
+      } else if (curr[sort] > next[sort]) {
+        return 1;
+      } else {
+        return 0;
+      }
+    });
+  }
+
+  return res.send(results);
+});
+
+app.listen(8000, () => {
+  console.log('your server is up on port 8000 jake');
+});
